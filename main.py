c580de36d10ff558594ae15cbe35d2cc712b3edc
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-04-24 00:38:30 -0400

    added home page api in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import auth, todos
+from starlette import status
 
 app = FastAPI()
 
@@ -10,3 +11,7 @@ models.Base.metadata.create_all(bind = engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 
+
+@app.get("/", status_code=status.HTTP_200_OK)
+def welcome_msg():
+    return {"Greet": "Welcome! This is your todo app."}
