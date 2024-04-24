a8ad64298756e354018214a582381274159d835e
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-04-24 16:23:56 -0400

    Added commented lines on how to use mysql and postgres instead of sqlite

--- database.py
+++ database.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqltodosapp.db"
 
+# SQLALCHEMY_DATABASE_URL_PG = "postgresql://user:pass@localhost/sqltodosapp"
+# SQLALCHEMY_DATABASE_URL_MYSQL = "mysql+pymysql://user:pass@localhost/sqltodosapp"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread":False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
