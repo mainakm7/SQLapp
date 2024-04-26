8e86b4b683911cde1b39f71c0dcb2dc4a416ad16
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-04-26 13:26:16 -0400

    Changed database from sqlite to MySQL
    Started using production based database like MuSQL instead of sqlite for
    scaling up. Most of the code for database table creation is the same.
    Added String length for compatibility with MySQL
    A MySQL table creation code file is also added but not used

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-# from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sqltodosapp.db"
+SQLALCHEMY_DATABASE_URL_sqlite = "sqlite:///./sqltodosapp.db"
 
 # SQLALCHEMY_DATABASE_URL_PG = "postgresql://user:pass@localhost/sqltodosapp"
-# SQLALCHEMY_DATABASE_URL_MYSQL = "mysql+pymysql://user:pass@localhost/sqltodosapp"
+SQLALCHEMY_DATABASE_URL_MYSQL = "mysql+pymysql://root:test1234@localhost:8080/sqltodosapp"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread":False})
+# engine = create_engine(SQLALCHEMY_DATABASE_URL_sqlite, connect_args = {"check_same_thread":False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL_MYSQL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- models.py
+++ models.py
@@ -5,8 +5,8 @@ class Todos(Base):
     __tablename__ = "todos"
     
     id = Column(Integer, primary_key = True, index = True)
-    title = Column(String)
-    description = Column(String)
+    title = Column(String(200))
+    description = Column(String(200))
     priority = Column(Integer)
     complete = Column(Boolean,default = False)
     owner_id = Column(Integer, ForeignKey("users.id"))
@@ -17,12 +17,12 @@ class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key = True, index = True)
-    email = Column(String, unique=True)
-    username = Column(String, unique=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    hashed_password = Column(String)
+    email = Column(String(200), unique=True)
+    username = Column(String(45), unique=True)
+    first_name = Column(String(45))
+    last_name = Column(String(45))
+    hashed_password = Column(String(200))
     is_active = Column(Boolean, default=True)
-    role = Column(String)
+    role = Column(String(45))
     
     
