de7ad69a61e2352f85daf5cb0cfa68e243da3daf
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-04-29 23:03:58 -0400

    For admin unit tests changed name of the test functions for admin role only

--- test/test_admin.py
+++ test/test_admin.py
@@ -8,14 +8,14 @@ app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
 
 
-def test_read_todos(test_todo):
+def test_read_todos_admin_authenticated(test_todo):
     response = client.get("/admin/todo")
     
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [{"complete": False, "title": "App", "description": "testing apps", "id":1, "priority": 5, "owner_id": 1}]
     
     
-def test_delete_todos(test_todo):
+def test_delete_todos_admin_authenticated(test_todo):
     response = client.delete("/admin/delete_todo/1")
     
     assert response.status_code == status.HTTP_204_NO_CONTENT
@@ -24,7 +24,7 @@ def test_delete_todos(test_todo):
     model = db.query(Todos).filter(Todos.id==1).first()
     assert model == None
     
-def test_delete_todos_not_found(test_todo):
+def test_delete_todos_not_found_admin_authenticated(test_todo):
     response = client.delete("/admin/delete_todo/999")
     
     assert response.status_code == status.HTTP_404_NOT_FOUND
